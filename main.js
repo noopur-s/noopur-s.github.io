49b4f69d7d5d65c26731c293558ec7bd75d4f4e8
noopur-s <33737004+noopur-s@users.noreply.github.com>
2017-11-16 15:59:53 -0800

    add space

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function math() {
   var answer = prompt ("enter 1+1:");
   if (answer == 2){
-    confirm ("you got it right.  your answer was"  +  answer);
+    confirm ("you got it right.  your answer was "  +  answer);
     }
     else {
       confirm("you got it wrong");
