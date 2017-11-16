dcb45fc919eccce26625bd5626a449f38a20378e
noopur-s <33737004+noopur-s@users.noreply.github.com>
2017-11-16 15:41:05 -0800

    created math function

---
+++ main.js
@@ -0,0 +1,9 @@
+function math() {
+  var answer = prompt ("enter 1+1:");
+  if (answer == 2){
+    confirm ("you got it right. your answer was" + answer);
+    }
+    else {
+      confirm("you got it wrong");
+   }
+  }
